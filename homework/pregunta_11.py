e05a5aa133d4b6255e9fa44c7694b00894501b69
JJMunera <jumunerap@unal.edu.co>
2025-06-17 12:42:06 -0500

    labo2
    intento1

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -4,10 +4,21 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_11():
-    """
+     tbl0 = pd.read_csv('../files/input/tbl1.tsv', sep='\t')
+     
+     resultado = (
+          tbl0.groupby('c0')['c4']
+          .apply(lambda x: ','.join(sorted(map(str, x))))
+          .reset_index() 
+     )
+     
+     return resultado
+
+
+"""
     Construya una tabla que contenga `c0` y una lista separada por ',' de
     los valores de la columna `c4` del archivo `tbl1.tsv`.
 
@@ -21,4 +32,4 @@ def pregunta_11():
     37   37  a,c,e,f
     38   38      d,e
     39   39    a,d,f
-    """
+"""
